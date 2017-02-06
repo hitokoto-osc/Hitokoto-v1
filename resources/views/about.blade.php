1f018c61e733883f2f8975c5d7139199644ae2e1
kuertianshi <i@loli.online>
2017-02-07 00:07:10 +0800

    修改Api&关于一言介绍页面

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,15 +9,15 @@
                 </div>
                 <h3>About 一言网</h3>
                 <h4>这个网站是干什么的？</h4>
-                <p style="text-decoration:line-through">
-                    一言网创立于2016年，隶属于萌创Team，目前网站主要提供一句话服务。
+                <p>
+                    一言网(Hitokoto.cn)创立于2016年，隶属于萌创Team，目前网站主要提供一句话服务。
                 </p>
                 <p>
                     动漫也好、小说也好、网络也好，不论在哪里，我们总会看到有那么一两个句子能穿透你的心。我们把这些句子汇聚起来，形成一言网络，以传递更多的感动。如果可以，我们希望我们没有停止服务的那一天。
                 </p>
                 <p>
                     简单来说，一言指的就是一句话，可以是动漫中的台词，也可以是网络上的各种小段子。<br/>或是感动，或是开心，有或是单纯的回忆。来到这里，留下你所喜欢的那一句句话，与大家分享，这就是一言存在的目的。*<br/>
-                    *:本段文本源自hitokoto.us,目前已经有一段时间无法访问了.
+                    *:本段文本源自hitokoto.us.
                 </p>
                 <h4>我可以干什么呢？</h4>
                 <p>
@@ -38,6 +38,10 @@
                     <a class="mdl-button mdl-js-button mdl-button--accent" href="http://blog.lwl12.com/">LWL的自由天空
                         以及其一言Api项目</a>
                     <a class="mdl-button mdl-js-button mdl-button--accent" href="http://www.mypcqq.cc/">MyPCQQ</a>
+                </p>
+                <h4>赞助</h4>
+                <p>
+                    <a class="mdl-button mdl-js-button mdl-button--accent" href="http://loongcat.lofter.com">龙猫〆沧笙踏歌</a>
                 </p>
             </div>
         </div>
--- resources/views/api.blade.php
+++ resources/views/api.blade.php
@@ -9,27 +9,27 @@
                 </div>
                 <h3>一言网 Api 接口说明</h3>
                 <h4>1、说明</h4>
-                <p style="text-decoration:line-through">
-                    一言网创立于2016年，隶属于萌创Team，目前网站主要提供一句话服务。
+                <p>
+                    一言网(Hitokoto.cn)创立于2016年，隶属于萌创Team，目前网站主要提供一句话服务。
                 </p>
                 <p>
                     动漫也好、小说也好、网络也好，不论在哪里，我们总会看到有那么一两个句子能穿透你的心。我们把这些句子汇聚起来，形成一言网络，以传递更多的感动。如果可以，我们希望我们没有停止服务的那一天。
                 </p>
                 <p>
-                    简单来说，一言指的就是一句话，可以是动漫中的台词，也可以是网络上的各种小段子。<br/>或是感动，或是开心，有或是单纯的回忆。来到这里，留下你所喜欢的那一句句话，与大家分享，这就是一言存在的目的。*<br/>
-                    *:本段文本源自hitokoto.us,目前已经有一段时间无法访问了.
+                    简单来说，一言指的就是一句话，可以是动漫中的台词，也可以是网络上的各种小段子。<br/>或是感动，或是开心，有或是单纯的回忆。来到这里，留下你所喜欢的那一句句话，与大家分享，这就是一言存在的目的。<br/>
+                    *:本段文本源自hitokoto.us
 
                 </p>
                 <h4>2、Api</h4>
                 <p>
                     请求地址：<br/>
                     HTTP : http://api.hitokoto.cn/<br/>
-                    SSL : https://sslapi.hitokoto.cn/
+                    SSL(推荐) : https://sslapi.hitokoto.cn/
                 </p>
                 <h4>3、参数</h4>
                 <p>
 
-                <table border="0" cellspacing="0" cellpadding="0">
+                <table border="0.5" cellspacing="0" cellpadding="0">
                     <tbody>
                     <tr>
                         <th width="100"><b>参数名称</b></th>
@@ -70,14 +70,27 @@
                         <td colspan="2">Other – 其他</td>
                     </tr>
                     <tr>
-                        <td>其他未定义的<br/>没有此参数</td>
+                        <td>其他不存在参数</td>
                         <td colspan="2">任意类型随机取得</td>
                     </tr>
                     <tr>
-                        <td style="text-align: center;">text</td>
-                        <td style="text-align: center;">可选</td>
-                        <td colspan="4">当此参数存在的时候，输出纯净句子。<br/>此参数不存在的时候，输出全部信息并进行编码。</td>
+                        <td rowspan="9" style="text-align: center;">encode</td>
+                        <td rowspan="9" style="text-align: center;">可选</td>
+
                     </tr>
+                    <tr>
+                        <td>text</td>
+                        <td colspan="2">返回纯净文本</td>
+                    </tr>
+                    <tr>
+                        <td>json</td>
+                        <td colspan="2">返回不进行unicode转码的json文本</td>
+                    </tr>
+                    <tr>
+                        <td>其他不存在参数</td>
+                        <td colspan="2">返回unicode转码的json文本</td>
+                    </tr>
+
                     </tbody>
                 </table>
 
@@ -114,15 +127,15 @@
                         <td>添加时间。</td>
                     </tr>
                     <tr>
-                        <td colspan="2">注意：如果text参数不为空，那么输出的只有一言正文。</td>
+                        <td colspan="2">注意：如果encode参数为text，那么输出的只有一言正文。</td>
                     </tr>
                     </tbody>
                 </table>
                 <h4>5、示例</h4>
                 <p>
-                    http://api.hitokoto.cn（从7种分类中随机抽取）<br/><br/>
-                    http://api.hitokoto.cn/?c=b （请求获得一个分类是漫画的句子）<br/><br/>
-                    http://api.hitokoto.cn/?c=f&text=1 （请求获得一个来自网络的句子，并以纯文本格式输出）
+                    https://sslapi.hitokoto.cn/（从7种分类中随机抽取）<br/><br/>
+                    https://sslapi.hitokoto.cn/?c=b （请求获得一个分类是漫画的句子）<br/><br/>
+                    https://sslapi.hitokoto.cn/?c=f&encode=text （请求获得一个来自网络的句子，并以纯文本格式输出）
                 </p>
             </div>
         </div>
