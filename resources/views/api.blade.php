253c1f78fe757abc950a2986b0ea2fa0939ec270
a632079 <a632079@qq.com>
2018-03-05 21:06:11 +0800

    Update api.blade.php

--- resources/views/api.blade.php
+++ resources/views/api.blade.php
@@ -159,6 +159,44 @@
                     https://v1.hitokoto.cn/?c=b （请求获得一个分类是漫画的句子）<br/><br/>
                     https://v1.hitokoto.cn/?c=f&encode=text （请求获得一个来自网络的句子，并以纯文本格式输出）
                 </p>
+                <code><pre><p id="hitokoto">:D 获取中...</p>
+<!-- 以下写法，选取一种即可 -->
+
+<!-- 现代写法，推荐 -->
+<!-- 兼容低版本浏览器 (包括 IE)，可移除 -->
+<script src="https://cdn.bootcss.com/bluebird/3.5.1/bluebird.core.min.js"></script>
+<script src="https://cdn.bootcss.com/fetch/2.0.3/fetch.min.js"></script>
+<!--End-->
+<script>
+  fetch('https://sslapi.hitokoto.cn')
+    .then(function (res){
+      return res.json();
+    })
+    .then(function (data) {
+      var hitokoto = document.getElementById('hitokoto');
+      hitokoto.innerText = data.hitokoto; 
+    })
+    .catch(function (err) {
+      console.error(err);
+    })
+</script>
+
+<!-- 老式写法，兼容性最忧 -->
+<script>
+  var xhr = new XMLHttpRequest();
+  xhr.open('get', 'https://sslapi.hitokoto.cn');
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      var data = JSON.parse(xhr.responseText);
+      var hitokoto = document.getElementById('hitokoto');
+      hitokoto.innerText = data.hitokoto;
+    }
+  }
+  xhr.send();
+</script>
+
+<!-- 新 API 方法， 十分简洁 -->
+<script src="https://v1.hitokoto.cn/?encode=js&select=%23hitokoto" defer></script></pre></code>
             </div>
         </div>
         <footer class="mdl-mini-footer">
